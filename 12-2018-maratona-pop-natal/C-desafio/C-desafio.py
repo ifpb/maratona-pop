7dc6a816e42dcae1311658580e461cefd59cbf1a
Calebe Oliveira <30943380+calebeof@users.noreply.github.com>
2018-12-29 03:13:50 -0200

    Solução da C

---
+++ 12-2018-maratona-pop-natal/C-desafio/C-desafio.py
@@ -0,0 +1,3 @@
+lista = ['IFPB', 'PASSAS','FAMILIA', 'PANETONE', 'ELFO', 'PERU', 'POPNATAL', 'ARVORE', 'ROBERTOCARLOS', 'PRESEPIO', 'PRESENTE','PAPAINOEL', 'NEVE', 'SIMONE', 'SALPICAO', 'MAMAENOEL', 'CHOCOLATE', 'DUENDE', 'GRINCH','ESTRELA']
+for i in range (0, len(lista)):
+  print('{}: {}'.format(i+1, lista[i]))
